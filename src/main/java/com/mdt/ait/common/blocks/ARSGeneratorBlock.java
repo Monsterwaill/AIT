364a795b96f065cf80303f4724364ca1afa32b7a
Duzo <james.hall01@outlook.com>
2023-02-22 20:58:33 +0000

    Added info text to some complicated blocks

--- src/main/java/com/mdt/ait/common/blocks/ARSGeneratorBlock.java
+++ src/main/java/com/mdt/ait/common/blocks/ARSGeneratorBlock.java
@@ -7,15 +7,24 @@ import com.mdt.ait.core.init.AITItems;
 import com.mdt.ait.tardis.structures.BaseStructure;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.UUID;
 
 import static com.mdt.ait.common.blocks.ARSProducerBlock.checkHeldItem;
@@ -26,6 +35,12 @@ public class ARSGeneratorBlock extends Block {
 
     public ARSGeneratorBlock(Properties p_i48440_1_) {
         super(p_i48440_1_);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_190948_1_, @Nullable IBlockReader p_190948_2_, List<ITextComponent> pTooltip, ITooltipFlag p_190948_4_) {
+        super.appendHoverText(p_190948_1_, p_190948_2_, pTooltip, p_190948_4_);
+        pTooltip.add(new TranslationTextComponent("Right click with SONIC to use!").setStyle(Style.EMPTY.withItalic(true).withColor(TextFormatting.GREEN)));
     }
 
     @Override
--- src/main/java/com/mdt/ait/common/blocks/ARSRemoverBlock.java
+++ src/main/java/com/mdt/ait/common/blocks/ARSRemoverBlock.java
@@ -9,18 +9,25 @@ import com.mdt.ait.core.init.AITTiles;
 import com.mdt.ait.tardis.structures.BaseStructure;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.UUID;
 
 import static com.mdt.ait.common.blocks.ARSProducerBlock.checkHeldItem;
@@ -30,6 +37,12 @@ public class ARSRemoverBlock extends Block {
 
     public ARSRemoverBlock(Properties p_i48440_1_) {
         super(p_i48440_1_);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_190948_1_, @Nullable IBlockReader p_190948_2_, List<ITextComponent> pTooltip, ITooltipFlag p_190948_4_) {
+        super.appendHoverText(p_190948_1_, p_190948_2_, pTooltip, p_190948_4_);
+        pTooltip.add(new TranslationTextComponent("WIP | BROKEN FUNCTION").setStyle(Style.EMPTY.withItalic(true).withColor(TextFormatting.RED)));
     }
 
     @Nullable
@@ -43,6 +56,7 @@ public class ARSRemoverBlock extends Block {
         return true;
     }
 
+    // @TODO Fix ARS remover.
     @Override
     public ActionResultType use(BlockState pState, World pLevel, BlockPos pPos, PlayerEntity pPlayer, Hand pHand, BlockRayTraceResult pHit) {
         if (checkHeldItem(pPlayer, AITItems.TENNANT_SONIC.get()) || checkHeldItem(pPlayer, AITItems.WHITTAKER_SONIC.get())) {
